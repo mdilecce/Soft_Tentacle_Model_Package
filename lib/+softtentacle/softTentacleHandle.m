bccf442f106ff311baebc0c94d687dfc9337c66d
elmdl <elmdl@ELEC-PC-200507.ds.leeds.ac.uk>
2022-10-11 18:00:42 +0100

    Adding Class Soft Link

---
+++ lib/+softtentacle/softTentacleHandle.m
@@ -0,0 +1,16 @@
+classdef softTentacleHandle < handle
+% Brief: Handler for Subclasses derivated
+% Details:
+%    Handler to gain access to methods and property of the subclass
+%    softLink adn softTentacle
+
+% Author:                          Michele Di Lecce
+% Email:                           elmdl@leeds.ac.uk,michele.dilecce@hotmail.com
+% Created:                         11-Oct-2022 16:40:01
+% Version history revision notes:
+%                                  None
+% Implementation In Matlab R2022a
+% Copyright © 2022 University of Leeds.All Rights Reserved.
+% 
+    
+end
